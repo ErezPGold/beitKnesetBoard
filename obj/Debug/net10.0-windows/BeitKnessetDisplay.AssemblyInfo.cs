0e9ecd5c08ef6b68f35d20b5e7dc361eda9f0851
Erez <erezp.770@gmail.com>
2026-06-15 12:15:37 +0300

    add halachos

--- obj/Debug/net10.0-windows/BeitKnessetDisplay.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/BeitKnessetDisplay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeitKnessetDisplay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ed9e7df96494dd49461cef84045ea6f4293479")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945f6a65e30bed3833778132ddcb27894a5d3a74")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeitKnessetDisplay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeitKnessetDisplay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
