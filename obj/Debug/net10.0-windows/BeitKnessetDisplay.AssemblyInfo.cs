5a091eebfd0094c3f068c5ad90595c3d2fe39769
Erez <erezp.770@gmail.com>
2026-06-26 14:03:43 +0300

    update 26_6_26

--- obj/Debug/net10.0-windows/BeitKnessetDisplay.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/BeitKnessetDisplay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeitKnessetDisplay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+585b41b4bc2a66d56935b7495acd3165dde1132c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb115e4b74dd70cbeb6cd94695e546cb784ed26")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeitKnessetDisplay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeitKnessetDisplay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
